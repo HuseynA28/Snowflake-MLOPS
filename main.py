8065d6d3310abbfd2710b1056f0bb034a9b0d941
Huseyn Abdullayev <55749285+HuseynA28@users.noreply.github.com>
2024-07-03 21:44:28 +0000

    updated

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ from dotenv import load_dotenv
 import os
 
 app = FastAPI()
-
-# Load environment variables from .env file
 load_dotenv()
 
-# Configuration for Snowflake connection
 connection_parameters = {
     "account": os.getenv("SNOWFLAKE_ACCOUNT"),
     "user": os.getenv("SNOWFLAKE_USER"),
@@ -21,10 +18,7 @@ connection_parameters = {
     "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
 }
 
-# Create Snowflake session
 session = Session.builder.configs(connection_parameters).create()
-
-# Redirect the root URL to the FastAPI documentation
 @app.get("/", include_in_schema=False)
 def read_root():
     return RedirectResponse(url='/docs')
@@ -32,15 +26,12 @@ def read_root():
 @app.get("/predict/")
 def predict(tv: float, radio: float, newspaper: float):
     try:
-        # Define the DataFrame
         advertising_df = session.create_dataframe([[tv, radio, newspaper]], schema=["TV", "RADIO", "NEWSPAPER"])
         
-        # Call the UDF from Snowflake
         predicted_sales_df = advertising_df.select(
             call_udf("predict_sales", col("TV"), col("RADIO"), col("NEWSPAPER")).alias("PREDICTED_SALES")
         )
-        
-        # Fetch the results
+
         result = predicted_sales_df.collect()
         
         return {"predicted_sales": result[0]["PREDICTED_SALES"]}
